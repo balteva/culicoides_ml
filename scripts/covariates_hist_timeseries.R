ad96863574665227695b4c0afc00ecca3a2e23a1
Ieva BALTUSYTE <ieva.baltusyte@cirad.fr>
2025-02-03 13:44:03 +0000

    Replace covariates_hist_timeseries.R

--- scripts/covariates_hist_timeseries.R
+++ scripts/covariates_hist_timeseries.R
@@ -1,8 +1,7 @@
 library(fst)
 library(tidyverse)
 library(lubridate)
-library(gridExtra)
-library(dplyr)
+library(ggplot2) 
 
 # output_file <- ("C:/Users/ibalt/OneDrive/Desktop/df_short.csv")
 # write.csv(df_short ,file=output_file, row.names = FALSE)
@@ -27,7 +26,7 @@ data <- subset(data_na, !NBINDIV_na)
 #selecting columns of interest
 df_short <- as.data.frame(data) %>%
   select(ID_SITE, DATE, NBINDIV,ESPECE,ECO_CLI, longitude, latitude) %>%
-  filter(NBINDIV != 0) %>%
+#  filter(NBINDIV != 0) %>%
   mutate(DATE = as.Date(DATE))
 
 
@@ -36,7 +35,7 @@ df_short <- as.data.frame(data) %>%
 #adding up all species per day per site 
 
 df_MY<- df_short%>%
-  group_by(DATE,ID_SITE) %>% #,ECO_CLI) %>%
+  group_by(DATE,ID_SITE,ECO_CLI) %>%
   summarize(NBINDIV_per_trap=sum(NBINDIV), .groups="drop")
 ---------------------------------------------------------------------------------------------------------------
 
@@ -64,7 +63,7 @@ df_MY_no$month <- as.factor(month(df_MY_no$DATE))
 ##simple histogram for all years as counts per night per trap
 df_MY_no%>%
   ggplot(aes(NBINDIV_per_trap)) +
-  geom_histogram(fill="cornflowerblue", binwidth = 50, col = "black", alpha= 0.7)+
+  geom_histogram(fill="cornflowerblue", binwidth = 10, col = "black", alpha= 0.7)+
   geom_vline(aes(xintercept = mean(NBINDIV_per_trap), color = "mean"), linetype = "solid", size = 1)+ #adding descriptive stats
   geom_vline(aes(xintercept = median(NBINDIV_per_trap), color = "median"), linetype = "solid", size = 1)+
 #  facet_grid(~year(DATE))+ 
@@ -75,7 +74,7 @@ df_MY_no%>%
   scale_color_manual(name = "statistics", values = c(median = "blue", mean = "red"))+
   theme_minimal()
 
-dev.off()
+#dev.off()
 
 
 
@@ -89,7 +88,7 @@ df_MY_no$year <- as.factor(year(df_MY_no$DATE))
 #jpeg("../../hist_pertrap_pernight_with_stats_cornflower_indiv_years.jpeg")
 df_MY_no%>%
   ggplot(aes(NBINDIV_per_trap)) +
-  geom_histogram(fill="cornflowerblue", binwidth = 50, col = "black", alpha= 0.7)+
+  geom_histogram(fill="cornflowerblue", binwidth = 10, col = "black", alpha= 0.7)+
   geom_vline(data = stats_df, aes(xintercept = mean_value, color = "mean"), linetype = "solid", size = 0.5)+ #adding descriptive stats
   geom_vline(data = stats_df, aes(xintercept = median_value, color = "median"), linetype = "solid", size = 0.5)+
   facet_wrap(~year)+ 
@@ -105,23 +104,23 @@ df_MY_no%>%
 
 ##with eco_clim
 
-#df_MY_no$year <- as.factor(year(df_MY_no$DATE))
-# jpeg("../../hist_pertrap_pernight_with_stats_cornflower_indiv_years_eco_cli.jpeg")
-# df_MY_no%>%
-#   ggplot(aes(NBINDIV_per_trap)) +
-#   geom_histogram(aes(fill=ECO_CLI), binwidth = 50, col = "black", alpha= 0.7)+
-# #  geom_vline(data = stats_df, aes(xintercept = mean_value, color = "mean"), linetype = "solid", size = 0.5)+ #adding descriptive stats
-# #  geom_vline(data = stats_df, aes(xintercept = median_value, color = "median"), linetype = "solid", size = 0.5)+
-#   facet_wrap(~year)+ 
-#   ggtitle("Histogram of culicoides spp. counts per trap per night 2009-2012\n (outliers excluded)") +
-#   theme(plot.title = element_text(hjust = 0.5))+
-#   labs(x="indiv. per trap per night",
-#        y="Frequency",
-#        fill="Ecoclimatic Zone")+
-#   scale_color_manual(name = "statistics", values = c(median = "blue", mean = "red"))+
-#   theme_minimal()
-# 
-# dev.off()
+df_MY_no$year <- as.factor(year(df_MY_no$DATE))
+jpeg("../../hist_pertrap_pernight_all_spp_no_outliers_fw_eco_clim_month.jpeg.jpeg")
+df_MY_no%>%
+  ggplot(aes(NBINDIV_per_trap)) +
+  geom_histogram(aes(fill=ECO_CLI), binwidth = 50, col = "black", alpha= 0.7)+
+#  geom_vline(data = stats_df, aes(xintercept = mean_value, color = "mean"), linetype = "solid", size = 0.5)+ #adding descriptive stats
+#  geom_vline(data = stats_df, aes(xintercept = median_value, color = "median"), linetype = "solid", size = 0.5)+
+  facet_grid(~ECO_CLI~year)+
+  ggtitle("Histogram of culicoides spp. counts per trap per night 2009-2012\n (outliers excluded)") +
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(x="indiv. per trap per night",
+       y="Frequency",
+       fill="Ecoclimatic Zone")
+#  scale_color_manual(name = "statistics", values = c(median = "blue", mean = "red"))+
+#  theme_minimal()
+
+dev.off()
 
 ################
 ----------------------------------------------------------------------------------------------------------------
@@ -129,14 +128,14 @@ df_MY_no%>%
 ----------------------------------------------------------------------------------------------------------------
   
 df_MY_sum <- df_MY %>%
-  group_by(DATE)%>%#,ECO_CLI) %>%
+  group_by(DATE,ECO_CLI) %>%
   summarise(NBINDIV_per_trap=sum(NBINDIV_per_trap), .groups="drop")
-library(ggplot2) 
 
-#jpeg("../../temporal_simple_cumulative.jpeg")
+
+jpeg("../../temporal_simple_cumulative.jpeg")
 
 #temporal changes - cumulative daily samples (culicoides spp combined)
-ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap))+ #,color=ECO_CLI)) +
+#ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap))+#color=ECO_CLI)) +
   geom_line() +
 #  facet_wrap(~ECO_CLI) +
   labs(title = "Cumulative Culicoides spp. counts 2009-2012 in France",
@@ -151,23 +150,26 @@ ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap))+ #,color=ECO_CLI)) +
 
 -----------------
   df_MY_sum <- df_MY %>%
-  group_by(DATE,ID_SITE)%>%#,ECO_CLI) %>%
+  group_by(DATE,ID_SITE,ECO_CLI)%>%#,ECO_CLI) %>%
   summarise(NBINDIV_per_trap=mean(NBINDIV_per_trap), .groups="drop")%>%
-  group_by(DATE)%>%#,ECO_CLI) %>%
+  group_by(DATE, ECO_CLI)%>%#,ECO_CLI) %>%
   summarise(samples=n(), NBINDIV_per_trap=sum(NBINDIV_per_trap)/samples, .groups="drop")
 
-#jpeg("../../temporal_simple_per_trap_per_day_mean.jpeg")
+
+  
+jpeg("../../temporal_eco_cli_per_trap_per_day_mean.jpeg")
 
 #temporal changes - all trap average for each day
-ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap))+ #,color=ECO_CLI)) +
+ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap,color=ECO_CLI)) +
   geom_line() +
   labs(title = "Trap sample mean of Culicoides counts/trap/day\n2009-2012 in France",
        x = "Timeline",
        y = "indiv. counted") +
   theme(plot.title = element_text(hjust = 0.5))+
   guides(color="none")+
+  facet_grid(~ECO_CLI)+
   theme_minimal()
-#dev.off()
+dev.off()
 
 
 
@@ -180,22 +182,22 @@ ggplot(df_MY_sum, aes(x=DATE, y=NBINDIV_per_trap))+ #,color=ECO_CLI)) +
 df_short_avg <- df_MY %>%
   mutate(year = year(DATE),
         month = month(DATE)) %>%
- group_by(ID_SITE, year, month) %>% #ECO_CLI
+ group_by(ID_SITE, year, month, ECO_CLI) %>% #ECO_CLI
 summarise(n_collection_pm = n(), Avg_pt_pm = sum(NBINDIV_per_trap)/n_collection_pm, .groups="drop") %>% 
-  group_by(year,month)%>%#this would give monthly avg. per unique trap per night #ECO_CLI
+  group_by(year,month, ECO_CLI)%>%#this would give monthly avg. per unique trap per night #ECO_CLI
 summarise(Avg_pt_pm = mean(Avg_pt_pm), .groups="drop") #this would give monthly avg. per trap per night (all traps combined)
 
 
-#jpeg("../../temporal_month_avg_per_trap_bars.jpeg")
+jpeg("../../temporal_month_avg_per_trap_eco_cli_bars.jpeg")
 #df_short_avg$month <- as.numeric(df_short_avg$month)
 
-ggplot(df_short_avg, aes(x = month, y = Avg_pt_pm))+ #color = ECO_CLI)) +    
- geom_line()+
-#  geom_bar(stat = "identity", position = "stack", width = 0.9) +    # if i want bar chart
-  facet_wrap(~year)+  
+ggplot(df_short_avg, aes(x = month, y = Avg_pt_pm, fill = ECO_CLI)) +    
+# geom_line()+
+  geom_bar(stat = "identity", position = "stack", width = 0.9) +    # if i want bar chart
+  facet_grid(~year)+  
   labs(title = "Indiv. caught per trap per night (month's average)",
        x = "Month",
-       y = "indiv./trap/night")+ #fill = "Ecoclimatic Zone")+
+       y = "indiv./trap/night",fill = "Ecoclimatic Zone")+
   scale_x_continuous(breaks = 1:12)
   theme(plot.title = element_text(hjust = 0.5))+
   theme_minimal()
@@ -218,19 +220,19 @@ df_short_avg <- df_MY %>%
   summarise(n_collection_pm = n(), Avg_pt_pm = sum(NBINDIV_per_trap)/n_collection_pm, .groups="drop") %>% 
   group_by(year,month,ECO_CLI)%>%
   summarise(Avg_pt_pm = mean(Avg_pt_pm), .groups="drop") 
-
-ggplot(df_short_avg, aes(x = month, y = Avg_pt_pm, fill = ECO_CLI)) +    
-#  geom_line()+
-  geom_bar(stat = "identity", position = "stack", width = 0.9) +    
+jpeg("../../temporal_month_avg_per_trap_eco_cli_line.jpeg")
+ggplot(df_short_avg, aes(x = month, y = Avg_pt_pm, color = ECO_CLI)) +    
+  geom_line()+
+#  geom_bar(stat = "identity", position = "stack", width = 0.9) +    
   facet_wrap(~year)+  
   labs(title = "Indiv. caught per trap per night (month's average)",
        x = "Month",
        y = "indiv./trap/night",
-       fill ="Ecoclimatic Zone")+ 
+       color ="Ecoclimatic Zone")+ 
   scale_x_continuous(breaks = 1:12)
 theme(plot.title = element_text(hjust = 0.5))+
   theme_minimal()
-
+dev.off()
 
   
   
